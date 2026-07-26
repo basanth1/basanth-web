ce6b9ca31186874fcd716c46e9708440b7e2cd8e
basanth1 <pbasanthkumar60@gmail.com>
2026-07-26 17:09:55 +0530

    Update Projects.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -11,6 +11,30 @@ export const Projects=()=>{
                 Featured Projects
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                        <img
+                            src="./projects/plant-ai.png"
+                            alt="Plant Disease Detection"
+                            className="rounded-lg mb-4 w-full h-40 object-cover"
+                        />
+                        <h3 className="text-xl font-bold mb-2">OCR Pipeline</h3>
+                        <p className="text-gray-400 mb-3">
+                            An AI-powered OCR platform that accurately extracts structured text from PDFs, scanned documents, and images using PaddleOCR and intelligent image preprocessing, with support for multiple export formats.
+                        </p>
+                        <div className="flex flex-wrap gap-2 mb-4">
+                            {["HuggingFace", "Python", "PaddleOCR", "Pdfplumber"].map((tech, key) => (
+                            <span
+                                key={key}
+                                className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                                {tech}
+                            </span>
+                             ))}
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <a href="https://huggingface.co/spaces/basanth1/OCR-pipeline" target="_blank" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project &#8594;</a>
+                        </div>
+                    </div>
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
                         <img
                             src="./projects/plant-ai.png"
